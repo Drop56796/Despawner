a1f3996d9f28fe6a21fdf319681764bf3f01f458
seek lol.lua <162717999+Drop56796@users.noreply.github.com>
2024-06-01 19:19:09 +0800

    Create Despawn.lua

---
+++ Despawn.lua
@@ -0,0 +1,31 @@
+local cue2 = Instance.new("Sound")
+	cue2.Parent = game.Workspace
+	cue2.Name = "Sound"
+	cue2.SoundId = "rbxassetid://1837829565"
+	cue2.Volume = 6
+	cue2.PlaybackSpeed = 0.7
+	cue2:Play()
+wait(1.01)
+local cue2 = Instance.new("Sound")
+	cue2.Parent = game.Workspace
+	cue2.Name = "Sound"
+	cue2.SoundId = "rbxassetid://1837829565"
+	cue2.Volume = 5
+	cue2.PlaybackSpeed = 0.6
+	cue2:Play()
+wait(1.01)
+local cue2 = Instance.new("Sound")
+	cue2.Parent = game.Workspace
+	cue2.Name = "Sound"
+	cue2.SoundId = "rbxassetid://1837829565"
+	cue2.Volume = 4
+	cue2.PlaybackSpeed = 0.5
+	cue2:Play()
+wait(1.01)
+local cue2 = Instance.new("Sound")
+	cue2.Parent = game.Workspace
+	cue2.Name = "Sound"
+	cue2.SoundId = "rbxassetid://1837829565"
+	cue2.Volume = 3
+	cue2.PlaybackSpeed = 0.4
+	cue2:Play()
